14ee3a34fc9acc3eef86b9c3546248c3bc3f183a
ryanperez473 <ryanperez473@gmail.com>
2020-11-02 08:38:06 +0800

    Log folder is removed from gitignore (it is being ignored as the log folder is usually ignored)

---
+++ Goldpoint Inventory System/Log/TransactDetails.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Goldpoint_Inventory_System.Log
+{
+    /// <summary>
+    /// Interaction logic for TransactDetails.xaml
+    /// </summary>
+    public partial class TransactDetails : UserControl
+    {
+        public TransactDetails()
+        {
+            InitializeComponent();
+            stack.DataContext = new ExpanderListViewModel();
+
+        }
+    }
+}
